49a264d00fce363e1cc97cf04fe5a38d32c60947
Md.Mohiul Islam Miraz <mohiulislam23@gmail.com>
2024-09-06 22:43:14 +0600

    Header

---
+++ static/assets/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function (){
+
+    /*----------------------------------------- header -----------------------------------------*/
+
+    window.addEventListener('scroll', function(){
+        const header = document.querySelector('header');
+
+        if (window.scrollY > 0) header.classList.add('scrolled');
+        else header.classList.remove('scrolled');
+    });
+});
