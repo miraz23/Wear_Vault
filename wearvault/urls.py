45efceabd74a29c88c197e9a98a3bd8b0e49e6b7
Md.Mohiul Islam Miraz <mohiulislam23@gmail.com>
2024-10-12 22:12:02 +0600

    About us

--- wearvault/urls.py
+++ wearvault/urls.py
@@ -21,4 +21,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
+    path('about', views.about, name="about"),
 ]
--- wearvault/views.py
+++ wearvault/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
