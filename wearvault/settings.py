5954f1bd69a8930bc9c4383ef2820ed417e50e09
Md.Mohiul Islam Miraz <mohiulislam23@gmail.com>
2024-11-02 16:35:35 +0600

    Dynamic Trending Section

--- wearvault/settings.py
+++ wearvault/settings.py
@@ -171,7 +171,7 @@ PAYPAL_CLIENT_SECRET = os.environ.get('PAYPAL_CLIENT_SECRET')
 
 
 paypalrestsdk.configure({
-    "mode": "live",
+    "mode": "sandbox",
     "client_id": PAYPAL_CLIENT_ID,
     "client_secret": PAYPAL_CLIENT_SECRET
 })
--- wearvault/views.py
+++ wearvault/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from contact.models import contact
-from shop.models import product
+from shop.models import product, order
+import json
 
 def index(request):
+    # Counts for each category
     drop_shoulders_count = product.objects.filter(product_category='drop-shoulders').count()
     baggy_joggers_count = product.objects.filter(product_category='baggy-joggers').count()
     baggy_shirts_count = product.objects.filter(product_category='baggy-shirts').count()
@@ -10,16 +12,31 @@ def index(request):
     head_wear_count = product.objects.filter(product_category='head-wear').count()
     baggy_shorts_count = product.objects.filter(product_category='baggy-shorts').count()
 
+    # Latest arrivals
     latest_arrivals = product.objects.filter(latest_arrival='yes')
 
-    data={
-        'drop_shoulders_count' : drop_shoulders_count,
-        'baggy_joggers_count' : baggy_joggers_count,
-        'baggy_shirts_count' : baggy_shirts_count,
-        'cargo_pants_count' : cargo_pants_count,
-        'head_wear_count' : head_wear_count,
-        'baggy_shorts_count' : baggy_shorts_count,
+    # Trending products
+    product_counts = {}
+    for ord in order.objects.all():
+        items = json.loads(ord.items_json)
+        for product_code, details in items.items():
+            product_counts[product_code] = product_counts.get(product_code, 0) + details[0]  # Counting quantities
+
+    # Sorted products by quantity in descending order and get top 6
+    top_product_codes = sorted(product_counts, key=product_counts.get, reverse=True)[:6]
+    
+    trending_products = product.objects.filter(id__in=[int(code[2:]) for code in top_product_codes])
+
+
+    data = {
+        'drop_shoulders_count': drop_shoulders_count,
+        'baggy_joggers_count': baggy_joggers_count,
+        'baggy_shirts_count': baggy_shirts_count,
+        'cargo_pants_count': cargo_pants_count,
+        'head_wear_count': head_wear_count,
+        'baggy_shorts_count': baggy_shorts_count,
         'latest_arrivals': latest_arrivals,
+        'trending_products': trending_products,
     }
 
     return render(request, "index.html", data)
