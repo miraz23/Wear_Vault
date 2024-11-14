89eb4abde0349b8011032bc056710d08de096667
Md.Mohiul Islam Miraz <mohiulislam23@gmail.com>
2024-11-14 16:01:32 +0600

    Bug Fixed-10

--- wearvault/views.py
+++ wearvault/views.py
@@ -35,35 +35,13 @@ def index(request, category=None):
     trending_products = product.objects.filter(id__in=[int(code.split('_')[0][2:]) for code in top_product_codes]).order_by(order_by_case)
 
     # Filtered Products
-    allProds = []
     if request.headers.get('x-requested-with') == 'XMLHttpRequest' and search_query:
-        # Filter products with partial match on search_query
         matching_products = product.objects.filter(
             Q(product_name__icontains=search_query)
         ).values('id', 'product_name', 'product_price', 'product_image_1')
-        
-        n = len(matching_products)
-        nSlides = n // 4 + ceil((n / 4) - (n // 4))
-        allProds.append([matching_products, range(1, nSlides), nSlides])
 
         data = {'results': list(matching_products)}
         return JsonResponse(data)
-    
-    else:
-        # Original code for category filtering or all products
-        if category:
-            prod = product.objects.filter(product_category=category)
-            n = len(prod)
-            nSlides = n // 4 + ceil((n / 4) - (n // 4))
-            allProds.append([prod, range(1, nSlides), nSlides])
-        else:
-            catprods = product.objects.values('product_category', 'id')
-            cats = {item['product_category'] for item in catprods}
-            for cat in cats:
-                prod = product.objects.filter(product_category=cat)
-                n = len(prod)
-                nSlides = n // 4 + ceil((n / 4) - (n // 4))
-                allProds.append([prod, range(1, nSlides), nSlides])
 
     data = {
         'drop_shoulders_count': drop_shoulders_count,
@@ -74,7 +52,6 @@ def index(request, category=None):
         'baggy_shorts_count': baggy_shorts_count,
         'latest_arrivals': latest_arrivals,
         'trending_products': trending_products,
-        'allProds': allProds,
     }
 
     return render(request, "index.html", data)
